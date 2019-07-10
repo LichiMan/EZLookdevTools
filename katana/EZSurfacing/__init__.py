02e6775ed40517e5e945ea41de32d2c1b14a222d
Ezequiel Mastrasso <ezequielm@gmail.com>
2019-07-10 22:23:13 +0100

    Katana EZCollections funcs
    Auto create collections for Surfacing Objects and Projects

---
+++ katana/EZSurfacing/__init__.py
@@ -0,0 +1,67 @@
+from Katana import Widgets, FnGeolib, Nodes3DAPI, NodegraphAPI
+
+def get_locations_hasattr(node, search_location, cel_expression):
+    '''Get all locations matching a cel_expression, at a location generated at a given
+    a node'''
+    collector = Widgets.CollectAndSelectInScenegraph(cel_expression, search_location)
+    matchedLocationPaths = collector.collectAndSelect(select=False, node= node)
+    return matchedLocationPaths 
+
+def get_attribute_values(node, attribute_locations, attribute_name):
+    '''Gets a list of values for the attribute_name, cooking the attribute locations'''
+    runtime = FnGeolib.GetRegisteredRuntimeInstance()
+    txn = runtime.createTransaction()
+    client = txn.createClient()
+    op = Nodes3DAPI.GetOp(txn, NodegraphAPI.GetNode(node.getName()))
+    txn.setClientOp(client, op)
+    runtime.commit(txn)
+
+    EZ_attribute_values = []
+
+    for location in attribute_locations:
+        cooked_location = client.cookLocation(location)
+        attrs = cooked_location.getAttrs()
+        attribute = attrs.getChildByName(attribute_name)
+        attribute_value = attribute.getChildByName('value').getValue()
+        if attribute_value not in EZ_attribute_values:
+            EZ_attribute_values.append (attribute_value)
+    return EZ_attribute_values
+
+def get_objects_attribute_values(node, attribute_name):
+    '''Lists EZSurfacing_objects at a given view node'''
+    #attribute_name = 'geometry.arbitrary.EZSurfacing_object'
+    #attribute_name = 'geometry.arbitrary.EZSurfacing_project'
+    cel_expression = '//*{ hasattr("%s") }' % attribute_name
+    search_location = '/root/world'
+    attribute_locations = get_locations_hasattr(node, search_location, cel_expression)
+    return get_attribute_values(node, attribute_locations, attribute_name)
+
+def add_node_to_group(group_node, node):
+    node.setParent(group_node)
+    node_inputPort = node.getInputPort('in')
+    node_outputPort = node.getOutputPort('out')
+    group_node_sendPort = group_node.getSendPort('in')
+    group_node_returnPort = group_node.getReturnPort('out')
+
+    last_out = group_node_returnPort.getConnectedPorts()[0]
+    last_out.connect(node_inputPort)
+    node_outputPort.connect(group_node_returnPort)
+
+def create_EZ_collections(attribute_name):
+    node = NodegraphAPI.GetAllSelectedNodes()
+    if len(node)!=1:
+       raise RuntimeError, 'Please select 1 node.'
+    rootNode = NodegraphAPI.GetRootNode()
+    EZSurfacing_projects = get_objects_attribute_values(node[0],attribute_name) 
+    group_stack = NodegraphAPI.CreateNode('GroupStack', rootNode)
+    group_stack.setName('EZPrjs')
+    for EZSurfacing_project in EZSurfacing_projects:
+        collection_create = NodegraphAPI.CreateNode('CollectionCreate', rootNode)
+        collection_create.setName('EZ%s' % EZSurfacing_project)
+        #collection_create_location = collection_create.getParameter('/')
+        #collection_create_location.setValue(location,0)
+        collection_create_name = collection_create.getParameter('name')
+        collection_create_name.setValue('EZ%s' % EZSurfacing_project,0)
+        collection_create_cel = collection_create.getParameter('CEL')
+        collection_create_cel.setValue('/root/world//*{attr("%s.value") == "%s"}' % (attribute_name, EZSurfacing_project),0)
+        add_node_to_group(group_stack, collection_create)
