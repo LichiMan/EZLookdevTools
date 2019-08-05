d5ef00f586f34c4ed5493ddf870033999e77ce14
Ezequiel Mastrasso <ezequielm@gmail.com>
2019-08-05 05:29:31 -0400

    resolutions half and third added

--- nuke/plugins/init.py
+++ nuke/plugins/init.py
@@ -11,8 +11,12 @@ nuke.knobDefault("Write.exr.compression","0")
 nuke.knobDefault("RotoPaint.toolbox", "brush {{brush ltt 0} {clone ltt 0}}")
 
 # Custom Resolutions
-nuke.addFormat( '1920 810 HD_810 (2.37)' )
-nuke.addFormat( '1920 817 HD_817 (2.35)' )
+nuke.addFormat( '1920 810 HD-2.37' )
+nuke.addFormat( '960 405 HD-2.37 half' )
+nuke.addFormat( '640 270 HD-2.37 third' )
+nuke.addFormat( '1920 817 HD-2.35' )
+nuke.addFormat( '960 408 HD-2.35 half' )
+nuke.addFormat( '640 272 HD-2.35 third' )
 
 # Project Settings > Default format: HD 1920x1080  
 nuke.knobDefault("Root.format", "HD")
