0751ccea0784e888cfa7097a76e8fc420ae41112
Ezequiel Mastrasso <ezequielm@gmail.com>
2019-08-14 00:13:09 -0400

    libs pyqt5 or pyside2

--- lookdevtools/ui/libs.py
+++ lookdevtools/ui/libs.py
@@ -6,3 +6,12 @@ try:
     from PySide2 import QtGui, QtWidgets, QtWidgets, QtUiTools, QtCore
 except:
     logging.error('UI Libs: Could not load PySide2')
+
+try:
+    logging.info('UI Libs: Loading PyQt5')
+    from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
+    from PyQt5 import QtGui, QtWidgets, QtWidgets, QtCore
+except:
+    logging.error('UI Libs: Could not load PyQt5')
+
+
