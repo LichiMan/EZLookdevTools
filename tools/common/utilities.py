18a16c4734837bb8eddc633cf70eee47f5c24425
anantgupta <anant1994@gmail.com>
2019-08-15 01:33:58 +0100

    merged master and cmake

--- tools/common/utilities.py
+++
@@ -1,8 +0,0 @@
-def get_random_color(seed):
-    random.seed(seed + "_r")
-    color_red = random.uniform(0, 1)
-    random.seed(seed + "_g")
-    color_green = random.uniform(0, 1)
-    random.seed(seed + "_b")
-    color_blue = random.uniform(0, 1)
-    return [color_red, color_green, color_blue]
